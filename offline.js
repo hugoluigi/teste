f6fdbd9bd36e184636d16ea76af10a6208dd4145
hugoluigi <hugoluigi@gmail.com>
2017-09-05 14:10:45 -0300

    aa
    aa

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1504590813,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/r1-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/pinata-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/base-sheet0.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
